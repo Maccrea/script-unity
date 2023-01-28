3d3ea661603526fde3906b8a08936ad8e2792d73
Maccrea <macreynardosan@gmail.com>
2023-01-28 22:28:57 +0700

    menambahkan punyanya handika

---
+++ CharControl(animator).cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharControl : MonoBehaviour
+{
+    public float kecepatan, scaleX, lompat, maxtinggi;
+    private Animator animator;
+
+    public KeyCode keyKanan = KeyCode.RightArrow, KeyCode keyKiri = KeyCode.LeftArrow, KeyCode keyLompat = KeyCode.UpArrow;
+    Rigidbody2D rb;
+    transform tr;
+
+    void Start()
+    {
+        scaleX = transform.localScale.x;
+
+        animator = GetComponent<Animator>();
+        tr = GetComponent<transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void jalan_kiri()
+    {
+        transform.localScale = new Vector3(-scaleX, transform.localScale.y,transform.localScale.z);
+        transform.Translate(Vector3.left * kecepatan * Time.fixedDeltaTime,Space.Self)
+        animator.SetBool("run", true);
+    }
+
+    void jalan_kanan()
+    {
+        transform.localScale = new Vector3(scaleX,transform.localScale.y, transform.localScale.z);
+        transform.Translate(Vector3.right * kecepatan* Time.fixedDeltaTime, Space.Self);
+        animator.SetBool("IsRun", true);
+    }
+
+    void berhenti()
+    {
+        animator.SetBool("IsRun", false);
+    }
+
+    void melompat()
+    {
+        GetComponent<Rigidbody2D>().velocity = new.Vector2(0, lompat);
+    }
+
+    void update()
+    {
+        if (Input.GetKey(keyKiri))
+        {
+            jalan_kiri
+        }
+        else if (Input.GetKey(keyKanan))
+        {
+            jalan_kanan
+        }
+        if (Input.GetKeyDown(keyLompat) && rb.velocity.y == 0)
+        {
+            melompat();
+        }
+
+        else if(Input.GetKeyUp(keyKiri) || Input.GetKeyUp(keyKanan))
+        {
+            berhenti();
+        }
+    }
+}
---
+++ CharControl.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharControl : MonoBehaviour
+{
+    public float kecepatan, scaleX, lompat, maxtinggi;
+    public KeyCode keyKanan = KeyCode.RightArrow, KeyCode keyKiri = KeyCode.LeftArrow, KeyCode keyLompat = KeyCode.UpArrow;
+    Rigidbody2D rb;
+    transform tr;
+
+    void Start()
+    {
+        scaleX = transform.localScale.x;
+
+        tr = GetComponent<transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void jalan_kiri()
+    {
+        transform.localScale = new Vector3(-scaleX, transform.localScale.y,transform.localScale.z);
+        transform.Translate(Vector3.left * kecepatan * Time.fixedDeltaTime,Space.Self)
+    }
+
+    void jalan_kanan()
+    {
+        transform.localScale = new Vector3(scaleX,transform.localScale.y, transform.localScale.z);
+        transform.Translate(Vector3.right * kecepatan* Time.fixedDeltaTime, Space.Self);
+    }
+
+    void melompat()
+    {
+        GetComponent<Rigidbody2D>().velocity = new.Vector2(0, lompat);
+    }
+
+    void update()
+    {
+        if (Input.GetKey(keyKiri))
+        {
+            jalan_kiri
+        }
+        else if (Input.GetKey(keyKanan))
+        {
+            jalan_kanan
+        }
+        if (Input.GetKeyDown(keyLompat)&& tr.position.y < maxtinggi)
+        {
+            melompat();
+        }
+    }
+}
