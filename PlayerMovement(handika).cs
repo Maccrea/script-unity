427aca282c0d2c1c7eff340e1c605d8103ba047d
Maccrea <macreynardosan@gmail.com>
2023-01-28 22:18:25 +0700

    menambahkan punyanya handika

--- PlayerMovement(handika).cs
+++ PlayerMovement(handika).cs
@@ -1,45 +1,52 @@
-public float kecepatan, scaleX, lompat, maxtinggi;
-public KeyCode keyKanan = KeyCode.RightArrow, KeyCode keyKiri = KeyCode.LeftArrow, KeyCode keyLompat = KeyCode.UpArrow;
-Rigidbody2D rb;
-transform tr;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-void Start()
+public class CharControl : MonoBehaviour
 {
-    scaleX = transform.localScale.x;
+    public float kecepatan, scaleX, lompat, maxtinggi;
+    public KeyCode keyKanan = KeyCode.RightArrow, KeyCode keyKiri = KeyCode.LeftArrow, KeyCode keyLompat = KeyCode.UpArrow;
+    Rigidbody2D rb;
+    transform tr;
 
-    tr = GetComponent<transform>();
-    rb = GetComponent<Rigidbody2D>();
-}
-
-void jalan_kiri()
-{
-    transform.localScale = new Vector3(-scaleX, transform.localScale.y,transform.localScale.z);
-    transform.Translate(Vector3.left * kecepatan * Time.fixedDeltaTime,Space.Self)
-}
-
-void jalan_kanan()
-{
-    transform.localScale = new Vector3(scaleX,transform.localScale.y, transform.localScale.z);
-    transform.Translate(Vector3.right * kecepatan* Time.fixedDeltaTime, Space.Self);
-}
-
-void melompat()
-{
-    GetComponent<Rigidbody2D>().velocity = new.Vector2(0, lompat);
-}
-
-void update()
-{
-    if (Input.GetKey(keyKiri))
+    void Start()
     {
-        jalan_kiri
+        scaleX = transform.localScale.x;
+
+        tr = GetComponent<transform>();
+        rb = GetComponent<Rigidbody2D>();
     }
-    else if (Input.GetKey(keyKanan))
+
+    void jalan_kiri()
     {
-        jalan_kanan
+        transform.localScale = new Vector3(-scaleX, transform.localScale.y,transform.localScale.z);
+        transform.Translate(Vector3.left * kecepatan * Time.fixedDeltaTime,Space.Self)
     }
-    if (Input.GetKeyDown(keyLompat)&& tr.position.y < maxtinggi)
+
+    void jalan_kanan()
     {
-        melompat();
+        transform.localScale = new Vector3(scaleX,transform.localScale.y, transform.localScale.z);
+        transform.Translate(Vector3.right * kecepatan* Time.fixedDeltaTime, Space.Self);
+    }
+
+    void melompat()
+    {
+        GetComponent<Rigidbody2D>().velocity = new.Vector2(0, lompat);
+    }
+
+    void update()
+    {
+        if (Input.GetKey(keyKiri))
+        {
+            jalan_kiri
+        }
+        else if (Input.GetKey(keyKanan))
+        {
+            jalan_kanan
+        }
+        if (Input.GetKeyDown(keyLompat)&& tr.position.y < maxtinggi)
+        {
+            melompat();
+        }
     }
 }
