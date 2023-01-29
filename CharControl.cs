2fef128076e1a67ea93cb82577b21ffa4f2544f6
Maccrea <91695776+Maccrea@users.noreply.github.com>
2023-01-29 11:23:40 +0700

    membenarkan untuk yang charcontrol

--- CharControl.cs
+++ CharControl.cs
@@ -4,49 +4,55 @@ using UnityEngine;
 
 public class CharControl : MonoBehaviour
 {
-    public float kecepatan, scaleX, lompat, maxtinggi;
-    public KeyCode keyKanan = KeyCode.RightArrow, KeyCode keyKiri = KeyCode.LeftArrow, KeyCode keyLompat = KeyCode.UpArrow;
+   public float kecepatan, scaleX, lompat, maxtinggi;
+    public KeyCode keyKanan = KeyCode.RightArrow, keyKiri = KeyCode.LeftArrow, keyLompat = KeyCode.UpArrow;
     Rigidbody2D rb;
-    transform tr;
+    Transform tr;
 
+    // Start is called before the first frame update
     void Start()
     {
         scaleX = transform.localScale.x;
 
-        tr = GetComponent<transform>();
+        tr = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     void jalan_kiri()
     {
-        transform.localScale = new Vector3(-scaleX, transform.localScale.y,transform.localScale.z);
-        transform.Translate(Vector3.left * kecepatan * Time.fixedDeltaTime,Space.Self)
+        transform.localScale = new Vector3(-scaleX, transform.localScale.y, transform.localScale.z);
+        transform.Translate(Vector3.left * kecepatan * Time.fixedDeltaTime, Space.Self);
+
+
     }
 
     void jalan_kanan()
     {
-        transform.localScale = new Vector3(scaleX,transform.localScale.y, transform.localScale.z);
-        transform.Translate(Vector3.right * kecepatan* Time.fixedDeltaTime, Space.Self);
+        transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
+        transform.Translate(Vector3.right * kecepatan * Time.fixedDeltaTime, Space.Self);
     }
 
     void melompat()
     {
-        GetComponent<Rigidbody2D>().velocity = new.Vector2(0, lompat);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, lompat);
     }
 
-    void update()
+
+    // Update is called once per frame
+    void Update()
     {
         if (Input.GetKey(keyKiri))
         {
-            jalan_kiri
+            jalan_kiri();
         }
         else if (Input.GetKey(keyKanan))
         {
-            jalan_kanan
+            jalan_kanan();
         }
-        if (Input.GetKeyDown(keyLompat)&& tr.position.y < maxtinggi)
+        if (Input.GetKeyDown(keyLompat) && tr.position.y < maxtinggi)
         {
             melompat();
+
         }
     }
 }
