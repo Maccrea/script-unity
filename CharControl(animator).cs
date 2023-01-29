6785f673295d6d2f7f59a976badaab64bb9a3370
Maccrea <91695776+Maccrea@users.noreply.github.com>
2023-01-29 12:25:56 +0700

    setelah perbaikan

--- CharControl(animator).cs
+++ CharControl(animator).cs
@@ -4,33 +4,34 @@ using UnityEngine;
 
 public class CharControl : MonoBehaviour
 {
-    public float kecepatan, scaleX, lompat, maxtinggi;
-    private Animator animator;
+   public float kecepatan, scaleX, lompat, maxtinggi;
+   private Animator animator;
 
-    public KeyCode keyKanan = KeyCode.RightArrow, KeyCode keyKiri = KeyCode.LeftArrow, KeyCode keyLompat = KeyCode.UpArrow;
+    public KeyCode keyKanan = KeyCode.RightArrow, keyKiri = KeyCode.LeftArrow, keyLompat = KeyCode.UpArrow;
     Rigidbody2D rb;
-    transform tr;
+    Transform tr;
 
+    // Start is called before the first frame update
     void Start()
     {
         scaleX = transform.localScale.x;
 
         animator = GetComponent<Animator>();
-        tr = GetComponent<transform>();
+        tr = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     void jalan_kiri()
     {
-        transform.localScale = new Vector3(-scaleX, transform.localScale.y,transform.localScale.z);
-        transform.Translate(Vector3.left * kecepatan * Time.fixedDeltaTime,Space.Self)
+        transform.localScale = new Vector3(-scaleX, transform.localScale.y, transform.localScale.z);
+        transform.Translate(Vector3.left * kecepatan * Time.fixedDeltaTime, Space.Self);
         animator.SetBool("run", true);
     }
 
     void jalan_kanan()
     {
-        transform.localScale = new Vector3(scaleX,transform.localScale.y, transform.localScale.z);
-        transform.Translate(Vector3.right * kecepatan* Time.fixedDeltaTime, Space.Self);
+        transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
+        transform.Translate(Vector3.right * kecepatan * Time.fixedDeltaTime, Space.Self);
         animator.SetBool("IsRun", true);
     }
 
@@ -41,18 +42,20 @@ public class CharControl : MonoBehaviour
 
     void melompat()
     {
-        GetComponent<Rigidbody2D>().velocity = new.Vector2(0, lompat);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, lompat);
     }
 
-    void update()
+
+    // Update is called once per frame
+    void Update()
     {
         if (Input.GetKey(keyKiri))
         {
-            jalan_kiri
+            jalan_kiri();
         }
         else if (Input.GetKey(keyKanan))
         {
-            jalan_kanan
+            jalan_kanan();
         }
         if (Input.GetKeyDown(keyLompat) && rb.velocity.y == 0)
         {
